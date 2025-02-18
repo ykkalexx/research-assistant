1502c3ee6bbccd418cf2852201e0c6da5fc5cf5c
alex-b23 <138300434+alex-b23@users.noreply.github.com>
2025-02-18 10:34:14 +0000

    [fix] Websockets error for Production

--- app/client/src/config/websocket.tsx
+++ app/client/src/config/websocket.tsx
@@ -25,7 +25,7 @@ export const WebSocketProvider = ({ children }: { children: ReactNode }) => {
   const [lastUpload, setLastUpload] = useState<UploadData | null>(null);
 
   useEffect(() => {
-    const socket = io("http://localhost:3000", {
+    const socket = io("https://research-assistant-production.up.railway.app", {
       withCredentials: true,
     });
 
