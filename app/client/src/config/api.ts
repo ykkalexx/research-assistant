2820384efa338b9b09eaeba4e0cf0bc76670d8fb
alex-b23 <138300434+alex-b23@users.noreply.github.com>
2025-02-18 10:19:42 +0000

    [feat] getting frontend ready for deployment

--- app/client/src/config/api.ts
+++ app/client/src/config/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://research-assistant-production.up.railway.app/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
